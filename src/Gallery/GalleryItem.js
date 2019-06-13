ffb272fa6574602ece6404487e0926d5aee6d95d
mmataja <mmataja92@gmail.com>
2019-06-13 17:23:07 +0200

    animation for down arrow changed

--- src/Gallery/GalleryItem.js
+++ src/Gallery/GalleryItem.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './GalleryItem.css';
 
 const GalleryItem = (props) => {
-    console.log("PROPS:", props);
     return (
-        <div className="gallery-img-container d-flex justify-content-center m-3">
+        <div className="gallery-img-container d-flex justify-content-center m-3 cursor-pointer">
             <img className="rounded img-fit" src={props.item.link} alt="slika"></img>
             <div className="text-over-img-container">
                 <div className="text-description">{props.item.description ? props.item.description : "Opis nedostupan"}</div>
