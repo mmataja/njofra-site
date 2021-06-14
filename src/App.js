a3fbb0f3a6df9b3f17f6e2a5b56453caa915165b
mmataja <mmataja92@gmail.com>
2021-06-14 12:15:20 +0200

    Test push

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 
 // *** libraries ***
 // import { Link, animateScroll as scroll } from 'react-scroll';
-import './fontAwesome';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Link } from 'react-scroll';
+import "./fontAwesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-scroll";
 
 // *** Components ***
-import Header from './Header/Header';
-import OurServices from './OurServices/OurServices';
-import ParallaxImage from './ParallaxImage/ParallaxImage';
-import AboutUs from './AboutUs/AboutUs';
-import Contact from './Contact/Contact';
-import Footer from './Footer/Footer';
-import Gallery from './Gallery/Gallery';
-
+import Header from "./Header/Header";
+import OurServices from "./OurServices/OurServices";
+import ParallaxImage from "./ParallaxImage/ParallaxImage";
+import AboutUs from "./AboutUs/AboutUs";
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
+import Gallery from "./Gallery/Gallery";
 
 class App extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className="App">
         <div className="parallax-image">
           <Header />
           <div className="h-35 d-flex justify-content-center align-items-center">
-            <Link 
-                activeClass="active"
-                to="services"
-                spy={true}
-                smooth={true}
-                duration={1000} 
+            <Link
+              activeClass="active"
+              to="services"
+              spy={true}
+              smooth={true}
+              duration={1000}
             >
               <h1 className="angle-down-color cursor-pointer angle-scroll-down p-3">
-                <FontAwesomeIcon icon="angle-down"/>
+                <FontAwesomeIcon icon="angle-down" />
+                test
               </h1>
             </Link>
           </div>
@@ -65,6 +64,6 @@ class App extends Component {
       </div>
     );
   }
-};
+}
 
 export default App;
